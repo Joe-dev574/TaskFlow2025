3c2ec5293f7975e94847480f5fac11b2c40730f2
Rangerboy574 <jdeweese574@icloud.com>
2025-04-11 21:34:22 -0500

    Add WelcomeScreen for onboarding integration

---
+++ TaskFlow/Views/OnBoarding/WelcomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeScreen.swift
+//  TaskFlow
+//
+//  Created by Joseph DeWeese on 4/11/25.
+//
+
+import SwiftUI
+
+struct WelcomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeScreen()
+}
