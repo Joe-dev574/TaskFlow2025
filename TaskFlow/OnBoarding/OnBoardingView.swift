245df82aea52cab255da213ad55670325d1dc1f1
Rangerboy574 <jdeweese574@icloud.com>
2025-04-11 21:00:37 -0500

    Add OnboardingView for onboarding integration

---
+++ TaskFlow/OnBoarding/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  TaskFlow
+//
+//  Created by Joseph DeWeese on 4/11/25.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
